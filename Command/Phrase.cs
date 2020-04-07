2e7acb93a17ba9ac21d399cecf5978a942e364cc
AvenNight <art-ibr@yandex.ru>
2020-04-08 02:06:32 +0700

    up

---
+++ Command/Phrase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+class Phrase : ICommand
+{
+    private string text;
+
+    public Phrase(string text)
+    {
+        this.text = text;
+    }
+
+    public async Task Send(Message message)
+    {
+        await Task.Delay(BotSetting.Delay);
+        await Bot.Current.SendTextMessageAsync(message.Chat.Id, text, replyToMessageId: message.MessageId);
+    }
+}
--- TriggersManager.cs
+++ TriggersManager.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
@@ -7,6 +10,7 @@ public static class TriggersManager
 	private static readonly ICommand smile = new Sticker(StickersID.Smile);
 	private static readonly ICommand cool = new Sticker(StickersID.Cool);
 	private static readonly ICommand question = new Sticker(StickersID.Question);
+	private static readonly ICommand nnnna = new Phrase(BotSetting.DaNa);
 	private static readonly ICommand phrases = new RndPhrase();
 
 	private static readonly Random rnd = new Random();
@@ -19,6 +23,8 @@ public static class TriggersManager
 				await cool.Send(message);
 			if (SmileTriggered(message.Text))
 				await smile.Send(message);
+			if (DaTriggered(message.Text))
+				await nnnna.Send(message);
 			if (QuestionTriggered(message.Text))
 				await question.Send(message);
 			if (Proc(BotSetting.PhraseChance))
@@ -38,14 +44,30 @@ public static class TriggersManager
 
 	private static bool SmileTriggered(string text)
 	{
+		return Triggered(text,
+			(text, trigger) => text.Contains(trigger, StringComparison.InvariantCultureIgnoreCase),
+			BotSetting.HahaTriggers,
+			BotSetting.SmileChance);
+	}
+
+	private static bool DaTriggered(string text)
+	{
+		return Triggered(text, 
+			(text, trigger) => text.Split().LastOrDefault().Equals(trigger, StringComparison.InvariantCultureIgnoreCase), 
+			BotSetting.DaTriggers, 
+			BotSetting.DaChance);
+	}
+
+	private static bool Triggered(string text, Func<string, string, bool> predicate, IEnumerable<string> triggers, double chance)
+	{
 		var contains = false;
-		foreach (var trigger in BotSetting.HahaTriggers)
-			if (text.Contains(trigger))
+		foreach (var trigger in triggers)
+			if (predicate(text, trigger))
 			{
 				contains = true;
 				break;
 			}
-		return contains && Proc(BotSetting.SmileChance);
+		return contains && Proc(BotSetting.DaChance);
 	}
 
 	private static bool Proc(double chance) => rnd.NextDouble() <= chance;
